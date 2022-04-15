ad3f8ff36b759bbea5461c5381f41d7162c20887
Danny Jiang <dannyjiang11@outlook.com>
2022-04-15 13:55:36 +0000

    added more functions

--- iss.js
+++ iss.js
@@ -14,4 +14,23 @@ const fetchMyIP = function(callback) {
   });
 };
 
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
+      return;
+    }
+
+    const { latitude, longitude } = JSON.parse(body);
+
+    callback(null, { latitude, longitude });
+  });
+};
+
 module.exports = { fetchMyIP };
+module.exports = { fetchCoordsByIP };
