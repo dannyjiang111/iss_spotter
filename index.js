e2711531a1681cb5d70deecceefd2bd8aa81ef3c
Danny Jiang <dannyjiang11@outlook.com>
2022-04-15 13:56:50 +0000

    added functions for ISS Spotter III

--- index.js
+++ index.js
@@ -19,3 +19,16 @@ fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
 
   console.log('It worked! Returned coordinates:' , coordinates);
 });
+
+const { fetchISSFlyOverTimes } = require('./iss');
+
+const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times:' , passTimes);
+});
